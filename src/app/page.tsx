42ca2e4a88e4d591232c58b36e783baa7dbd26de
OJ Abba <services@ojabba.com>
2026-09-22 15:09:16 +0100

    chore: add prettier support  & fix formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center">
@@ -6,5 +5,5 @@ export default function Home() {
         <h1>Typeahead</h1>
       </main>
     </div>
-  );
+  )
 }
