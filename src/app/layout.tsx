d36c25dcac29f8ab2386aa82da64d85f55a112b1
OJ Abba <services@ojabba.com>
2026-09-22 14:45:01 +0100

    chore: setup project environment

---
+++ src/app/layout.tsx
@@ -0,0 +1,18 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Typeahead",
+};
+
+export default function RootLayout(props: Readonly<LayoutProps<"/">>) {
+  const { children } = props
+  return (
+    <html
+      lang="en"
+      className={`h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
